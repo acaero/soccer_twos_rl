10a13cf80005d296e5057c476eea5ee3a27bd7c1
Lasse <lasse.friedrich.de@gmail.com>
2024-07-09 14:25:59 +0200

    mergy merge

--- src/runs/ppo_train.py
+++ src/runs/ppo_train.py
@@ -110,16 +110,21 @@ class ProgressBarManager(object):
 
 if __name__ == "__main__":
 
-    num_cpu = 4
+    num_cpu = 5
     vec_env = SubprocVecEnv([make_env(env_id=i + 1, rank=i) for i in range(num_cpu)])
 
-    model = PPO(
-        "MlpPolicy",
-        vec_env,
-        verbose=1,
-        tensorboard_log=LOG_DIR + "/tensorboard/",
-        device="auto",
+    # model = PPO(
+    #     "MlpPolicy",
+    #     vec_env,
+    #     verbose=1,
+    #     tensorboard_log=LOG_DIR + "/tensorboard/",
+    #     device="auto",
+    # )
+
+    model = PPO.load(
+        path=r"out\checkpoints\bestmodel_ppo.zip", env=vec_env, device="auto", verbose=1
     )
+    model.set_parameters(load_path_or_dict=r"out\checkpoints\bestmodel_ppo.zip")
 
     with ProgressBarManager(N_GAMES) as progress_callback:
         model.learn(N_GAMES, callback=[progress_callback])
