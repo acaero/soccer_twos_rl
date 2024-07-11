8872beb6f82194ac67ef1f969da07c8da3d9c477
Lasse <lasse.friedrich.de@gmail.com>
2024-07-11 10:53:04 +0200

    final cleanup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from src.runs.ddpg_train import train_ddpg
 from src.runs.dqn_train import train_dqn
 from src.runs.baseline_train import train_random, train_baseline
-from notebooks.tests.ppo_train_legacy import train_ppo
-from src.runs.a2c_train import train_a2c
 from src.config import N_GAMES
 
 if __name__ == "__main__":
@@ -10,5 +8,3 @@ if __name__ == "__main__":
     train_baseline(n_games=N_GAMES, n_agents=1)
     train_ddpg(n_games=N_GAMES, n_agents=1)
     train_dqn(n_games=N_GAMES, n_agents=1)
-    train_ppo(n_games=N_GAMES, n_agents=1)
-    train_a2c(n_games=N_GAMES, n_agents=1)
